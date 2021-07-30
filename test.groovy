3a435f8564db0893742ce96b2868ca65b95a6829
GangadharChowdam <88142081+GangadharChowdam@users.noreply.github.com>
2021-07-30 14:22:32 +0530

    Update test.groovy

--- test.groovy
+++ test.groovy
@@ -1,5 +1,31 @@
-def add(x, y){
-    return x +y
+public ArrayList<String> getEnvironments(String p_clientName) {
+
+    ArrayList<String> res = new ArrayList<String>();
+
+    if (!(p_clientName?.trim())){
+        res.add("Select a client");
+        return res;
+    }
+
+    def possibleEnvironments = yyz.getEnvironmentTypeEnum();
+
+    def requiredLabels = [new hudson.model.labels.LabelAtom ("PRODUCT")];
+    requiredLabels.add(new hudson.model.labels.LabelAtom(p_clientName.toUpperCase()));
+
+
+    Jenkins.instance.computers.each { node ->
+        if (node.assignedLabels.containsAll(requiredLabels)) {
+                // Yes.  Let's get the environment name out of it.
+                node.assignedLabels.any { al ->
+                    def e = yyz.getEnvironmentFromString(al.getName(), true);
+                    if (e != null) {
+                        res.add(al.getName());
+                        return; // this is a continue
+                    }
+                }
+            }
+        }
+
+
+    return res;
 }
-z = add(2, 3)
-println(z)
